db833a795ba56038259ffc8632d83a6e1b6e21e3
chabhishek123 <chabhishek123@gmail.com>
2015-12-30 08:21:00 -0800

    intro.py

---
+++ intro.py
@@ -0,0 +1,24 @@
+# Set the variables to the values listed in the instructions!
+my_int = 4
+my_float = 4.00
+compare = False
+if  4 == 4.00: 
+     compare = True
+print compare
+
+"""
+Variables, which store values for later use
+Data types, such as numbers and booleans
+Whitespace, which separates statements
+Comments, which make your code easier to read
+Arithmetic operations, including +, -, *, /, **, and %
+"""
+
+
+String oprs
+ name = raw_input("What is your name?")
+quest = raw_input("What is your quest?")
+color = raw_input("What is your favorite color?")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
